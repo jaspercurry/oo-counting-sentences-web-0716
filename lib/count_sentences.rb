8dbd046e13f636befe120ab7aa46e7ca577edf19
jaspercurry <jaspercurry@gmail.com>
2016-07-28 17:33:40 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    #self.split(/(?<=[?.!])/).count
+      self.split(/[?]|[!]|[.]/).delete_if { |ele| ele == ""}.count
   end
 end
+
+
+# http://stackoverflow.com/questions/18089562/how-do-i-keep-the-delimiters-when-splitting-a-ruby-string
+
+
